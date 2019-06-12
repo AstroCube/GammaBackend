9391dc62f068f0168c69338f411d09299bf7786a
Ian Castaño <ianfe@martina.com.co>
2019-06-11 22:21:34 -0500

    Updated friendsihp controller

--- controllers/friend.js
+++ controllers/friend.js
@@ -21,6 +21,53 @@ module.exports = {
     } else {
       return res.status(400).send({message: "No se ha enviado correctamente la petición."});
     }
+  },
+
+  checkFriendship: function(req, res) {
+    if (req.query.sender && req.query.receiver) {
+      Friend.findOne({$or: [
+          {sender: req.query.sender, receiver: req.query.receiver},
+          {sender: req.query.receiver, receiver: req.query.sender}]
+      }, (err, status) => {
+        if (err) return res.status(500).send({message: "Ha ocurrido un error al detectar la amistad."});
+        let finalStatus = false;
+        if (status) finalStatus = true;
+        return res.status(200).send({status: finalStatus});
+      });
+    } else {
+      return res.status(400).send({message: "No se han enviado los parámetros correctamente."});
+    }
+  },
+
+  removeFriendship: function(req, res) {
+    if (req.query.sender && req.query.receiver) {
+      Friend.findOneAndDelete({$or: [
+          {sender: req.query.sender, receiver: req.query.receiver},
+          {sender: req.query.receiver, receiver: req.query.sender}]
+      }, (err) => {
+        if (err) return res.status(500).send({message: "Ha ocurrido un error al detectar la amistad."});
+        return res.status(200).send({removed: true});
+      });
+    } else {
+      return res.status(400).send({message: "No se han enviado los parámetros correctamente."});
+    }
+  },
+
+  listFriends: function(req, res) {
+    Friend.find({$or: [{sender: req.params.id}, {receiver: req.params.id}]}, (err, friendList) => {
+       if (err) return res.status(500).send({message: "Ha ocurrido un error al obtener la lista de amigos."});
+       return res.status(200).send(friendList);
+    });
+  },
+
+  clearFriends: function(req, res) {
+    Friend.find({$or: [{sender: req.params.id}, {receiver: req.params.id}]}, async (err, friendList) => {
+      if (err) return res.status(500).send({message: "Ha ocurrido un error al obtener la lista de amigos."});
+      await Promise.forEach(friendList, (friend) => {
+        Friend.findOneAndDelete({_id: friend._id}, () => {});
+      });
+      return res.status(200).send({deleted: true});
+    });
   }
 
 };
--- routes/friend.js
+++ routes/friend.js
@@ -7,5 +7,9 @@ const FriendController = require("@friend_controller");
 let api = express.Router();
 
 api.post("/friend/create", cluster.ensureAuth, FriendController.createFriendship);
+api.get("/friend/check", cluster.ensureAuth, FriendController.checkFriendship);
+api.delete("/friend/delete", cluster.ensureAuth, FriendController.removeFriendship);
+api.get("/friend/list/:id", cluster.ensureAuth, FriendController.listFriends);
+api.delete("/friend/remove-all/:id", cluster.ensureAuth, FriendController.clearFriends);
 
 module.exports = api;
