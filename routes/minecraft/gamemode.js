e8caa83ce3a5c192e381a580de80d3075008f5aa
Ian Castaño <ianfe@martina.com.co>
2019-07-12 22:35:41 -0500

    Updated gamemode

--- routes/minecraft/gamemode.js
+++ routes/minecraft/gamemode.js
@@ -1,13 +1,12 @@
 "use strict";
 
 const express = require("express");
-const cluster_auth = require("@cluster_auth");
 const GamemodeController = require("@gamemode_controller");
 
 let api = express.Router();
 
 // -- Cluster Related -- //
-api.get("/gamemode/get/:id", cluster_auth.ensureAuth, GamemodeController.getGamemode);
-api.get("/gamemode/list", cluster_auth.ensureAuth, GamemodeController.listGamemodes);
+api.get("/gamemode/get/:id", GamemodeController.getGamemode);
+api.get("/gamemode/list", GamemodeController.listGamemodes);
 
 module.exports = api;
