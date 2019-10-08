6c66d72f80d69d3d229cfbed5751c5cea452abdc
TomateDeveloper <ianfe@martina.com.co>
2019-10-07 21:55:05 -0500

    Updated website GET match

--- routes/minecraft/match.js
+++ routes/minecraft/match.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const clusterAuth = require("@cluster_auth");
-const ingameAuth = require("@ingame_auth");
+const mdAuth = require("@default_auth");
 const MatchController = require("@match_controller");
 
 let api = express.Router();
@@ -10,6 +10,7 @@ let api = express.Router();
 api.post("/match/create", clusterAuth.ensureAuth, MatchController.matchCreate);
 api.post("/match/find", clusterAuth.ensureAuth, MatchController.matchFind);
 api.get("/match/get/:id", clusterAuth.ensureAuth, MatchController.matchGet);
+api.get("/match/get-website/:id", mdAuth.ensureAuth, MatchController.matchGet);
 api.put("/match/update/:id", clusterAuth.ensureAuth, MatchController.matchUpdate);
 api.get("/match/clean", clusterAuth.ensureAuth, MatchController.matchCleanup);
 
