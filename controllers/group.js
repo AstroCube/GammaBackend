cd1fdfae18ecd10138644da4a6487d4d26684078
TomateDeveloper <ianfe@martina.com.co>
2019-10-11 14:51:17 -0500

    Removed sensitive information

--- controllers/group.js
+++ controllers/group.js
@@ -18,7 +18,7 @@ module.exports = {
       if (!group) return res.status(404).send({message: "No se ha encontrado el grupo."});
       if (!group.staff) return res.status(403).send({message: "El grupo no es marcado como staff."});
 
-      User.find({group: {_id: req.params.id}}).select({password: 0}).exec((err, users) => {
+      User.find({group: {_id: req.params.id}}).select("_id username group last_seen twitter public_email").exec((err, users) => {
         if (err) return res.status(500).send({message: "Ha ocurrido un error al obtener los grupos."});
         return res.status(200).send(users);
       });
