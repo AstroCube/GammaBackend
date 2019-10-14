028e9445b971b1612941685a7629f77bd5f43ef8
TomateDeveloper <ianfe@martina.com.co>
2019-10-14 16:38:06 -0500

    Updated authentication type

--- routes/friend.js
+++ routes/friend.js
@@ -11,7 +11,7 @@ api.post("/friend/create", cluster.ensureAuth, FriendController.createFriendship
 api.get("/friend/check", cluster.ensureAuth, FriendController.checkFriendship);
 api.delete("/friend/delete", cluster.ensureAuth, FriendController.removeFriendship);
 api.get("/friend/list/:id", cluster.ensureAuth, FriendController.listFriends);
-api.get("/friend/list-website/:id", cluster.ensureAuth, FriendController.listFriendsWebsite);
+api.get("/friend/list-website/:id", mdAuth.ensureAuth, FriendController.listFriendsWebsite);
 api.delete("/friend/remove-all/:id", cluster.ensureAuth, FriendController.clearFriends);
 
 module.exports = api;
