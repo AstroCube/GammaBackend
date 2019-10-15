9910a968ec6cf3a7cd6fa8b542f880a1427443c8
TomateDeveloper <ianfe@martina.com.co>
2019-10-15 12:40:00 -0500

    Updated parameter

--- routes/minecraft/match.js
+++ routes/minecraft/match.js
@@ -11,7 +11,7 @@ api.post("/match/create", clusterAuth.ensureAuth, MatchController.matchCreate);
 api.post("/match/find", clusterAuth.ensureAuth, MatchController.matchFind);
 api.get("/match/get/:id", clusterAuth.ensureAuth, MatchController.matchGet);
 api.get("/match/get-website/:id", mdAuth.ensureAuth, MatchController.matchGetWebsite);
-api.get("/match/get-user/:id", mdAuth.ensureAuth, MatchController.matchGetPlayer);
+api.get("/match/get-user/:user", mdAuth.ensureAuth, MatchController.matchGetPlayer);
 api.put("/match/update/:id", clusterAuth.ensureAuth, MatchController.matchUpdate);
 api.get("/match/clean", clusterAuth.ensureAuth, MatchController.matchCleanup);
 
