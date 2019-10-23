2c92daeadd52b1962b2cbdb9f50b53c24870fa89
TomateDeveloper <ianfe@martina.com.co>
2019-10-23 15:10:59 -0500

    Updated mail server verify

--- routes/minecraft/user.js
+++ routes/minecraft/user.js
@@ -15,7 +15,7 @@ api.post("/user/login-server", cluster_auth.ensureAuth, minecraft_user_controlle
 api.put("/user/update-server/:id", cluster_auth.ensureAuth, minecraft_user_controller.user_update);
 api.get("/user/left-record", ingame_auth.ensureAuth, minecraft_user_controller.server_left);
 api.post("/user/email-register", cluster_auth.ensureAuth, minecraft_user_controller.server_email_register);
-api.get("/user/verify", ingame_auth.ensureAuth, minecraft_user_controller.server_email_verify);
+api.get("/user/verify", minecraft_user_controller.server_email_verify);
 api.post("/user/user-login", cluster_auth.ensureAuth, minecraft_user_controller.production_join);
 api.post("/user/get-permissions", cluster_auth.ensureAuth, minecraft_user_controller.user_permissions);
 api.post("/user/add-group", [cluster_auth.ensureAuth, ingame_auth.ensureAuth], minecraft_user_controller.group_add);
