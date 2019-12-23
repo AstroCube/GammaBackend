d71e80922cabec24f849558e58bdb95be71e887d
TomateDeveloper <ianfe@martina.com.co>
2019-12-22 22:24:33 -0500

    Updated authpas

--- utilities/redis_service.js
+++ utilities/redis_service.js
@@ -1,6 +1,10 @@
 "use strict";
 
 let redis_service = require('redis');
-let redisClient = redis_service.createClient(6379, '127.0.0.1');
+let redisClient = redis_service.createClient({
+    port: 6379,
+    host: '127.0.0.1',
+    auth_pass: "&g4MErwHo#Yx6A(RLw*Ci3K&[hyn%9A@"
+});
 
 exports.redisClient = redisClient;
